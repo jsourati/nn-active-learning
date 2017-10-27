a92a239102c0bf2dc5a498a41b5808af87513507
Jamshid Sourati <jamshid.sourati@childrens.harvard.edu>
2017-10-27 15:44:29 -0400

    Writing parameters of the experiment into a text file

--- run_experiments.sh
+++ run_experiments.sh
@@ -6,7 +6,6 @@ do
 done
 mkdir $exp_id
 
-
 # getting all the parameters
 runs=$1
 data_path=$2
@@ -15,13 +14,22 @@ k=$4
 max_queries=$5
 init_size=$6
 
+# creating a text file containing the parameters
+touch "$exp_id/parameters.txt"
+echo "runs=$runs" >> "$exp_id/parameters.txt"
+echo "data_path=$data_path" >> "$exp_id/parameters.txt"
+echo "target_classes_path=$target_classes_path" >> "$exp_id/parameters.txt"
+echo "k=$k" >> "$exp_id/parameters.txt"
+echo "max_queries=$max_queries" >> "$exp_id/parameters.txt"
+echo "init_size=$init_size" >> "$exp_id/parameters.txt"
+
+mkdir "$exp_id/saved_models"
+
 # in the following "i" is index of each run (run_id)
 for ((i=1;i<=$runs;i++))
 do
-    mkdir "$exp_id/saved_models"
     mkdir "$exp_id/$i"
     run_model_path="$exp_id/saved_models/init-$i.ckpt"
-    run_results_path="$exp_id/$i/results.dat"
     
     python run_querying_scr.py \
 	$exp_id \
