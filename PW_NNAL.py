Fixing entropy computation in US

PW_NNAL.py
@@ -35,12 +35,11 @@ def CNN_query(model,
         
     if method_name=='entropy':
         # posteriors
-        batch_size = 200
         posts = PW_NN.batch_eval(
             model, 
             pool_dict,
             patch_shape,
-            batch_size,
+            5000,
             stats,
             sess,
             'posteriors')[0]
@@ -50,9 +49,9 @@ def CNN_query(model,
         for path in list(posts.keys()):
             ttposts += list(posts[path])
             
-        # k most uncertain
-        q = np.argsort(np.array(
-            ttposts))[:qbatch_size]
+        # k most uncertain (binary classes)
+        q = np.argsort(np.abs(np.array(
+            ttposts)-.5))[:qbatch_size]
         
         q_dict = patch_utils.locate_in_dict(
             pool_dict, q)