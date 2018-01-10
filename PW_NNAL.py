d2bef2eeae8f82db0d9fdb69003620695c2f4f5c
Jamshid Sourati <jamshid.sourati@childrens.harvard.edu>
2018-01-10 14:12:54 -0500

    Initializing adding rep-entropy

--- PW_NNAL.py
+++ PW_NNAL.py
@@ -30,8 +30,7 @@ def CNN_query(model,
         q = np.random.permutation(n)[
             :qbatch_size]
 
-        q_dict = patch_utils.locate_in_dict(
-            pool_dict, q)
+        
         
     if method_name=='entropy':
         # posteriors
@@ -53,8 +52,80 @@ def CNN_query(model,
         q = np.argsort(np.abs(np.array(
             ttposts)-.5))[:qbatch_size]
         
-        q_dict = patch_utils.locate_in_dict(
-            pool_dict, q)
         
+    if method_name=='rep-entropy':
+        # posteriors
+        posts = PW_NN.batch_eval(
+            model, 
+            pool_dict,
+            patch_shape,
+            5000,
+            stats,
+            sess,
+            'posteriors')[0]
+        
+        # vectories everything
+        ttposts = []
+        for path in list(posts.keys()):
+            ttposts += list(posts[path])
+        
+        B = 1000
+        if B < len(ttposts):
+            sel_inds = binary_uncertainty_filter(
+                ttposts, B)
+            sel_ttposts = ttposts[sel_inds]
+        else:
+            B = len(ttposts)
+            sel_ttposts = ttposts
+            sel_inds = np.arange(B)
+            
+        n = len(ttposts)
+        rem_inds = list(set(np.arange(n)) - set(sel_inds))
+        
+        # extract the features for all the pool
+        # sel_inds, rem_inds  -->  pool_inds
+        F = model.extract_features(pool_inds,
+                                   expr,
+                                   session)
+        
+
+    # returning the sub-dictionary
+    q_dict = patch_utils.locate_in_dict(
+        pool_dict, q)
+    
     return q_dict
 
+
+def binary_uncertainty_filter(posts, B):
+    """Uncertainty filtering for binary class
+    label distribution
+    
+    Since there are only two classes, posterior
+    probability of only one of the classes
+    are given in form of 1D array.
+    """
+    
+    return np.argsort(np.abs(
+        np.array(posts)-0.5))[:B]
+
+def extract_features(model,
+                     pool_dict, 
+                     inds,
+                     stats,
+                     sess):
+    """Extracting features for some patches
+    that are indexed from within a dictionary
+    """
+    
+    # make a sub-dictionary for given indices
+    inds_dict = patch_utils.locate_in_dict(
+            pool_dict, inds)
+    sub_dict = {}
+    for path in list(inds_dict.keys()):
+        sub_dict[path] = pool_dict[path][
+            inds_dict[path]]
+        
+    # start computing the features
+    batches = patch_utils.get_batches(
+        sub_dict,1000)
+    
