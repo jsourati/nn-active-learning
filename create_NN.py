6742f4037f9e6e9cb72d1a73efbef88f6680e373
Jamshid Sourati <jamshid.sourati@childrens.harvard.edu>
2018-10-28 11:45:51 -0400

    Building Tiramisu 103-layer model

--- create_NN.py
+++ create_NN.py
@@ -193,3 +193,138 @@ def DenseNet_2block(growth_rate,
                             [[12, 26, 28], 0.2])
 
     return model
+
+def FCDenseNet_103Layers(input_shape, c, model_name):
+    """Also known as, Tiramisu network with 
+    103 layers
+    """
+    
+    # growth rate
+    k = 16
+
+    # first layer
+    pw_dict = {'first': ['conv', [48, [3,3]], 'BMA']}
+
+    
+    ''' DB+TD  '''
+    # number of Dense block layers
+    Ls = [4, 5, 7, 10, 12]
+    for i in range(len(Ls)):
+        # dense block
+        DB = {'DB%d_%d'%(i,j): ['conv', [k, [3,3]], 'BAM']
+              for j in range(Ls[i])}
+        pw_dict.update(DB)
+        # transition down
+        nfmap = 48+np.sum(Ls[:i+1])*k
+        TD = {'T_%d'%i: ['conv', [nfmap, [3,3]], 'BMA'],
+              'pool_%d'%i: ['pool', [2,2]]}
+        pw_dict.update(TD)
+
+    ''' Bottleneck Dense Block''' 
+    L = 15
+    BT = {'BottleDB_%d'%j: ['conv', [k, [3,3]], 'BAM']
+          for j in range(L)}
+    pw_dict.update(BT)
+
+    ''' TU+DB '''
+    Ls = np.flip(Ls)
+    for i in range(len(Ls)):
+        # transition up
+        nfmap = Ls[i]*k
+        TU = {'TU_%d'%i: ['conv_transpose', 
+                          [nfmap, [3,3], [2,2]], 'M']}
+        pw_dict.update(TU)
+        # dense block
+        DB = {'DB%d_%d'%(5+i,j): ['conv', [k, [3,3]], 'BAM']
+              for j in range(Ls[i])}
+        pw_dict.update(DB)
+
+    # last layer
+    pw_dict.update({'last': ['conv', [c,[1,1]], 'M']})
+
+
+    ''' Establishing the Skip Connections '''
+    stype = 'con'    # concatenation
+    skips = []
+
+    # intra-DB skip connections
+    # downward and upward paths
+    nDB = 9
+    layer_names = np.array(list(pw_dict.keys()))
+    for i in range(nDB):
+        # starting index
+        start_ind = np.where('DB%d_0'%i==layer_names)[0][0]
+        # number of layers
+        L = np.sum(['DB%d'%i in name for 
+                    name in layer_names])
+        for j in range(L):
+            skips += [[start_ind-1+j,
+                       list(np.arange(start_ind+1+j,
+                                      start_ind+L+1)),
+                       stype]]
+    # bottleneck DB
+    start_ind = np.where('BottleDB_0'==layer_names)[0][0]
+    L = np.sum(['BottleDB' in name for name in layer_names])
+    for j in range(L):
+        skips += [[start_ind-1+j, 
+                   list(np.arange(start_ind+1+j,
+                                  start_ind+L+1)),
+                  stype]]
+
+    # inter-DB skip connections
+    skipped_nodes = np.array([skips[i][0] for i in
+                              range(len(skips))])
+    # NOTE: we know that output nodes of DBs in downward
+    # path are not already skipped somewhere else, hence
+    # we don't need to check with an if
+    # output of DB4 --> output of TU_0 (input of DB5_0)
+    DB4_end_node = np.where(layer_names=='DB4_11')[0][0]
+    DB5_start_node = np.where(layer_names=='DB5_0')[0][0]
+    skips += [[DB4_end_node, [DB5_start_node], stype]]
+
+    # output of DB3 --> output of TU_1 (input of DB6_0)
+    DB3_end_node = np.where(layer_names=='DB3_9')[0][0]
+    DB6_start_node = np.where(layer_names=='DB6_0')[0][0]
+    skips += [[DB3_end_node, [DB6_start_node], stype]]
+
+    # output of DB2 --> output of TU_2 (input of DB7_0)
+    DB2_end_node = np.where(layer_names=='DB2_6')[0][0]
+    DB7_start_node = np.where(layer_names=='DB7_0')[0][0]
+    skips += [[DB2_end_node, [DB7_start_node], stype]]
+
+    # output of DB1 --> output of TU_3 (input of DB8_0)
+    DB1_end_node = np.where(layer_names=='DB1_4')[0][0]
+    DB8_start_node = np.where(layer_names=='DB8_0')[0][0]
+    skips += [[DB1_end_node, [DB8_start_node], stype]]
+
+    # output of DB0 --> output of TU_4 (input of DB9_0)
+    DB0_end_node = np.where(layer_names=='DB0_3')[0][0]
+    DB9_start_node = np.where(layer_names=='DB9_0')[0][0]
+    skips += [[DB0_end_node, [DB9_start_node], stype]]
+
+    # sorting the skips in terms of the sources
+    sort_inds = np.argsort([skips[i][0] for i in range(len(skips))])
+    sorted_skips = []
+    for ind in sort_inds:
+        sorted_skips += [skips[ind]]
+
+        ''' Specifying Drop-out Layers '''
+    # including all the layers, except max-pooling and
+    # upscaling layers
+    dp_layers = np.arange(len(pw_dict)).tolist()
+    no_dp = ['pool_0', 'pool_1', 'pool_2', 'pool_3',
+             'pool_4', 'TU_0', 'TU_1', 'TU_2', 'TU_3',
+             'TU_4']
+    for name in no_dp:
+        loc = np.where(layer_names==name)[0][0]
+        dp_layers.remove(loc)
+    dp_rate = 0.2
+
+    
+    ''' Creating the Model '''
+    x = tf.placeholder(tf.float32, [None,]+input_shape)
+    model = NN_extended.CNN(x, pw_dict, model_name, 
+                            sorted_skips,None,
+                            [dp_layers, dp_rate]) 
+
+    return model
